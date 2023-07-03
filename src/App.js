794eceeb1fefc4624b1f2295198e66f57f9da12a
aroojfatima41 <arooj.fatima@emumba.com>
2023-07-03 13:16:00 +0500

    Moved decision maker to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css"
 import ChoicesContainer from "./ChoicesContainer"
 import LifeLinesContainer from "./LifelinesContainer"
 import InputContainer from "./InputContainer"
-import Winner from "./Winner"
 import { useState } from "react"
 
 const App = () => {
@@ -12,14 +11,55 @@ const App = () => {
   const [moves, setMoves] = useState(10)
   const [computerScore, setComputerScore] = useState(0)
   const [playerScore, setPlayerScore] = useState(0)
-console.log(playerScore,computerScore)
+  const [result, setResult] = useState("")
+
+  const getComputerInput = () => {
+    const generateRandomNumber = Math.floor(Math.random() * 101)
+    return generateRandomNumber <= 33 ? "scissors" : generateRandomNumber <= 66 ? "rock" : "paper"
+  }
+  console.log(playerScore, computerScore)
+
+  const decision = (userChoice) => {
+    const computerChoice = getComputerInput()
+    setComputerChoice(computerChoice)
+    if (userChoice === computerChoice) {
+      setResult("Tie")
+    } else if (userChoice === "rock") {
+      if (computerChoice === "paper") {
+        setResult("Computer Won")
+        setComputerScore(computerScore + 1)
+      } else {
+        setResult("Player Won")
+        setPlayerScore(playerScore + 1)
+      }
+    } else if (userChoice === "scissors") {
+      if (computerChoice === "rock") {
+        setResult("Computer Won")
+        setComputerScore(computerScore + 1)
+      } else {
+        setResult("Player Won")
+        setPlayerScore(playerScore + 1)
+      }
+    } else if (userChoice === "paper") {
+      if (computerChoice === "scissors") {
+        setResult("Computer Won")
+        setComputerScore(computerScore + 1)
+      } else {
+        setResult("Player Won")
+        setPlayerScore(playerScore + 1)
+      }
+    }
+  }
+
   return (
     <div className="root">
       <h1 className="App">Rock Paper Scissor</h1>
 
       <LifeLinesContainer playerScore={playerScore} computerScore={computerScore} />
 
-      {userChoice && <InputContainer userInput={userChoice} loader={loader} computerChoice={computerChoice} />}
+      {userChoice && computerChoice && (
+        <InputContainer userInput={userChoice} loader={loader} computerChoice={computerChoice} />
+      )}
 
       <p>Moves Left {moves}</p>
 
@@ -29,20 +69,11 @@ console.log(playerScore,computerScore)
           setLoader={setLoader}
           setMoves={setMoves}
           moves={moves}
-          setComputerChoice={setComputerChoice}
+          onClick={decision}
         />
       )}
 
-      {moves !== 0 && !loader && userChoice && computerChoice && (
-        <Winner
-          player={userChoice}
-          computer={computerChoice}
-          playerScore={playerScore}
-          computerScore={computerScore}
-          setComputerScore={setComputerScore}
-          setPlayerScore={setPlayerScore}
-        />
-      )}
+      {moves !== 0 && !loader && userChoice && computerChoice && <h1>{result}</h1>}
 
       {moves === 0 && <h1>Game Over</h1>}
       {moves === 0 && (
--- src/ChoicesContainer/ChoicesContainer.js
+++ src/ChoicesContainer/ChoicesContainer.js
@@ -2,18 +2,13 @@ import { Button } from "antd"
 import { FaHandScissors, FaHandRock, FaHandPaper } from "react-icons/fa"
 import "./ChoicesContainer.css"
 
-const ChoicesCotainer = ({ setUserChoice, setLoader, setMoves, moves, setComputerChoice }) => {
-  const getComputerInput = () => {
-    const generateRandomNumber = Math.floor(Math.random() * 101)
-    return generateRandomNumber <= 33 ? "scissors" : generateRandomNumber <= 66 ? "rock" : "paper"
-  }
-
+const ChoicesCotainer = ({ setUserChoice, setLoader, setMoves, moves, onClick }) => {
   const onChoiceClick = (choice) => {
     if (moves !== 0) setMoves(moves - 1)
     setUserChoice(choice)
     setLoader(true)
     setTimeout(() => setLoader(false), 500)
-    setComputerChoice(getComputerInput())
+    onClick(choice)
   }
 
   return (
